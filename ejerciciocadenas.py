c3524c86c06dc50bd7cd72c3f8c315ffb541e2b5
Oscar <mao090997@gmail.com>
2026-01-09 21:57:53 -0500

    isograma

--- ejerciciocadenas.py
+++ ejerciciocadenas.py
@@ -84,3 +84,40 @@ print(s1.isdigit()) #si son digitos
 """
 EXTRA 
 """
+def check(word1: str, word2: str):
+    # Palindromos
+
+    print(f"¿{word1} es un palindromo?: {word1 == word1[::-1]}")
+    print(f"¿{word2} es un palindromo?: {word2 == word2[::-1]}")
+
+    # Anagramas
+
+    print(f"¿{word1} es anagrama de {word2}?: {sorted(word1) == sorted(word2)}")
+    
+    # Isogramas
+    def isogram(word: str) -> bool: 
+
+        word_dict = dict()
+
+        for character in word:
+         word_dict[character] = word_dict.get(character, 0) + 1
+
+        isograma = True
+        values = list(word_dict.values())
+        isograma_len = values[0]
+        for word_count in values:   
+          if word_count != isograma_len: 
+            isograma = False
+            break
+        return isograma
+
+    print(f"¿{word2} es un isograma?: {isogram(word2)}") 
+
+     
+    
+
+
+
+check("radar", "pythonpythonpythonpython")
+
+
